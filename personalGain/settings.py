3d201f81e67c2ba1fcc98963a967953af6b6af77
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-09-25 21:47:40 +0600

    csrf trusted origin added

--- personalGain/settings.py
+++ personalGain/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ['https://personal-gain.onrender.com','http://127.0.0.1:8000/']
 
 # Application definition
 
