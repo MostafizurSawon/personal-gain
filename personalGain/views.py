ae024504c75bb7e4cffe289fc0b37c67bb97b107
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-09-26 18:37:02 +0600

    home bug solved

--- personalGain/views.py
+++ personalGain/views.py
@@ -9,7 +9,7 @@ def home(request):
   tasks = Task.objects.all()
   # ac = UserAccount.objects.all()
   # print("home ac", ac['gender'])
-  user_account = request.user.account  # This retrieves the related UserAccount object
+  # user_account = request.user.account  # This retrieves the related UserAccount object
   # user_account = request.user.task_account  
 
-  return render(request, 'base.html', {"taskHome": tasks, "ac": user_account})
+  return render(request, 'base.html', {"taskHome": tasks})
